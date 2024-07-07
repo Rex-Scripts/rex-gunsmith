7ea4d551144ff044296723f38556492ddb369d07
RexshackGaming <rexshackuk@gmail.com>
2024-07-07 19:33:32 +0100

    config update

--- config.lua
+++ config.lua
@@ -104,7 +104,7 @@ Config.GunsmithLocations = {
 -- gunsmith crafting
 ---------------------------------
 Config.GunsmithCrafting = {
-
+    -- Revolvers
     {   category = 'Revolvers',
         crafttime = 30000,
         ingredients = {
@@ -169,6 +169,48 @@ Config.GunsmithCrafting = {
         receive = 'weapon_revolver_navy_crossover',
         giveamount = 1
     },
+    -- Revolver Ammo
+    {   category = 'Revolvers',
+        crafttime = 30000,
+        ingredients = {
+            [1] = { item = 'ammoparts', amount = 100 },
+        },
+        receive = 'ammo_revolver',
+        giveamount = 100
+    },
+    {   category = 'Revolvers',
+        crafttime = 30000,
+        ingredients = {
+            [1] = { item = 'ammoparts', amount = 100 },
+        },
+        receive = 'ammo_revolver_express',
+        giveamount = 100
+    },
+    {   category = 'Revolvers',
+        crafttime = 30000,
+        ingredients = {
+            [1] = { item = 'ammoparts', amount = 100 },
+        },
+        receive = 'ammo_revolver_express_explosive',
+        giveamount = 100
+    },
+    {   category = 'Revolvers',
+        crafttime = 30000,
+        ingredients = {
+            [1] = { item = 'ammoparts', amount = 100 },
+        },
+        receive = 'ammo_revolver_high_velocity',
+        giveamount = 100
+    },
+    {   category = 'Revolvers',
+        crafttime = 30000,
+        ingredients = {
+            [1] = { item = 'ammoparts', amount = 100 },
+        },
+        receive = 'ammo_revolver_split_point',
+        giveamount = 100
+    },
+    -- Pistols
     {   category = 'Pistols',
         crafttime = 30000,
         ingredients = {
@@ -201,6 +243,48 @@ Config.GunsmithCrafting = {
         receive = 'weapon_pistol_semiauto',
         giveamount = 1
     },
+    -- Pistol Ammo
+    {   category = 'Pistols',
+        crafttime = 30000,
+        ingredients = {
+            [1] = { item = 'ammoparts', amount = 100 },
+        },
+        receive = 'ammo_pistol',
+        giveamount = 100
+    },
+    {   category = 'Pistols',
+        crafttime = 30000,
+        ingredients = {
+            [1] = { item = 'ammoparts', amount = 100 },
+        },
+        receive = 'ammo_pistol_express',
+        giveamount = 100
+    },
+    {   category = 'Pistols',
+        crafttime = 30000,
+        ingredients = {
+            [1] = { item = 'ammoparts', amount = 100 },
+        },
+        receive = 'ammo_pistol_express_explosive',
+        giveamount = 100
+    },
+    {   category = 'Pistols',
+        crafttime = 30000,
+        ingredients = {
+            [1] = { item = 'ammoparts', amount = 100 },
+        },
+        receive = 'ammo_pistol_high_velocity',
+        giveamount = 100
+    },
+    {   category = 'Pistols',
+        crafttime = 30000,
+        ingredients = {
+            [1] = { item = 'ammoparts', amount = 100 },
+        },
+        receive = 'ammo_pistol_split_point',
+        giveamount = 100
+    },
+    -- Repeater
     {   category = 'Repeater',
         crafttime = 30000,
         ingredients = {
@@ -233,6 +317,48 @@ Config.GunsmithCrafting = {
         receive = 'weapon_repeater_evans',
         giveamount = 1
     },
+    -- Repeater Ammo
+    {   category = 'Repeater',
+        crafttime = 30000,
+        ingredients = {
+            [1] = { item = 'ammoparts', amount = 100 },
+        },
+        receive = 'ammo_repeater',
+        giveamount = 100
+    },
+    {   category = 'Repeater',
+        crafttime = 30000,
+        ingredients = {
+            [1] = { item = 'ammoparts', amount = 100 },
+        },
+        receive = 'ammo_repeater_express',
+        giveamount = 100
+    },
+    {   category = 'Repeater',
+        crafttime = 30000,
+        ingredients = {
+            [1] = { item = 'ammoparts', amount = 100 },
+        },
+        receive = 'ammo_repeater_express_explosive',
+        giveamount = 100
+    },
+    {   category = 'Repeater',
+        crafttime = 30000,
+        ingredients = {
+            [1] = { item = 'ammoparts', amount = 100 },
+        },
+        receive = 'ammo_repeater_high_velocity',
+        giveamount = 100
+    },
+    {   category = 'Repeater',
+        crafttime = 30000,
+        ingredients = {
+            [1] = { item = 'ammoparts', amount = 100 },
+        },
+        receive = 'ammo_repeater_split_point',
+        giveamount = 100
+    },
+    -- Rifle
     {   category = 'Rifle',
         crafttime = 30000,
         ingredients = {
@@ -265,7 +391,7 @@ Config.GunsmithCrafting = {
         receive = 'weapon_rifle_elephant',
         giveamount = 1
     },
-    {   category = 'Sniper Rifle',
+    {   category = 'Rifle',
         crafttime = 30000,
         ingredients = {
             [1] = { item = 'rifleparts', amount = 1 },
@@ -273,7 +399,7 @@ Config.GunsmithCrafting = {
         receive = 'weapon_sniperrifle_rollingblock',
         giveamount = 1
     },
-    {   category = 'Sniper Rifle',
+    {   category = 'Rifle',
         crafttime = 30000,
         ingredients = {
             [1] = { item = 'rifleparts', amount = 1 },
@@ -281,7 +407,7 @@ Config.GunsmithCrafting = {
         receive = 'weapon_sniperrifle_rollingblock_exotic',
         giveamount = 1
     },
-    {   category = 'Sniper Rifle',
+    {   category = 'Rifle',
         crafttime = 30000,
         ingredients = {
             [1] = { item = 'rifleparts', amount = 1 },
@@ -289,6 +415,72 @@ Config.GunsmithCrafting = {
         receive = 'weapon_sniperrifle_carcano',
         giveamount = 1
     },
+    -- Rifle Ammo
+    {   category = 'Rifle',
+        crafttime = 30000,
+        ingredients = {
+            [1] = { item = 'ammoparts', amount = 100 },
+        },
+        receive = 'ammo_rifle',
+        giveamount = 100
+    },
+    {   category = 'Rifle',
+        crafttime = 30000,
+        ingredients = {
+            [1] = { item = 'ammoparts', amount = 100 },
+        },
+        receive = 'ammo_rifle_express',
+        giveamount = 100
+    },
+    {   category = 'Rifle',
+        crafttime = 30000,
+        ingredients = {
+            [1] = { item = 'ammoparts', amount = 100 },
+        },
+        receive = 'ammo_rifle_express_explosive',
+        giveamount = 100
+    },
+    {   category = 'Rifle',
+        crafttime = 30000,
+        ingredients = {
+            [1] = { item = 'ammoparts', amount = 100 },
+        },
+        receive = 'ammo_rifle_high_velocity',
+        giveamount = 100
+    },
+    {   category = 'Rifle',
+        crafttime = 30000,
+        ingredients = {
+            [1] = { item = 'ammoparts', amount = 100 },
+        },
+        receive = 'ammo_rifle_split_point',
+        giveamount = 100
+    },
+    {   category = 'Rifle',
+        crafttime = 30000,
+        ingredients = {
+            [1] = { item = 'ammoparts', amount = 100 },
+        },
+        receive = 'ammo_rifle_elephant',
+        giveamount = 100
+    },
+    {   category = 'Rifle',
+        crafttime = 30000,
+        ingredients = {
+            [1] = { item = 'ammoparts', amount = 100 },
+        },
+        receive = 'ammo_varmint',
+        giveamount = 100
+    },
+    {   category = 'Rifle',
+        crafttime = 30000,
+        ingredients = {
+            [1] = { item = 'ammoparts', amount = 100 },
+        },
+        receive = 'ammo_varmint_tranquilizer',
+        giveamount = 100
+    },
+    -- Shotgun
     {   category = 'Shotgun',
         crafttime = 30000,
         ingredients = {
@@ -336,6 +528,39 @@ Config.GunsmithCrafting = {
         },
         receive = 'weapon_shotgun_repeating',
         giveamount = 1
+    },
+    -- Shotgun Ammo
+    {   category = 'Shotgun',
+        crafttime = 30000,
+        ingredients = {
+            [1] = { item = 'ammoparts', amount = 100 },
+        },
+        receive = 'ammo_shotgun',
+        giveamount = 100
+    },
+    {   category = 'Shotgun',
+        crafttime = 30000,
+        ingredients = {
+            [1] = { item = 'ammoparts', amount = 100 },
+        },
+        receive = 'ammo_shotgun_buckshot_incendiary',
+        giveamount = 100
+    },
+    {   category = 'Shotgun',
+        crafttime = 30000,
+        ingredients = {
+            [1] = { item = 'ammoparts', amount = 100 },
+        },
+        receive = 'ammo_shotgun_slug',
+        giveamount = 100
+    },
+    {   category = 'Shotgun',
+        crafttime = 30000,
+        ingredients = {
+            [1] = { item = 'ammoparts', amount = 100 },
+        },
+        receive = 'ammo_shotgun_slug_explosive',
+        giveamount = 100
     },
 
 }
